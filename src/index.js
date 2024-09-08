a01790358b07705c998dae382d2adfdd77cf01f6
Maria Mayowa <129012596+mariamayowa@users.noreply.github.com>
2024-09-08 22:35:55 +0100

    Added weather data functionality.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temp");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
+    console.log(response.data)
+
+    
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()},`;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+ 
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
