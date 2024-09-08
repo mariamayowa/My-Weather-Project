3d8ddee55c1d92a82157129ebd0fe49bee7625eb
Maria Mayowa <129012596+mariamayowa@users.noreply.github.com>
2024-09-08 23:28:47 +0100

    Simplified the date and time functions.

--- src/index.js
+++ src/index.js
@@ -8,16 +8,37 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
 
-    console.log(response.data)
+    console.log(response.data);
 
     
     cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()},`;
+    timeElement.innerHTML = `${formatDate(date)},`;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
  
-    temperatureElement.innerHTML = Math.round(temperature);
+    temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+}
+
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday",  
+        "Thursday", 
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){
