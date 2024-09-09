7de4ed14de3ef11f668ed0a3b3b5fc2c41b8b720
Maria Mayowa <129012596+mariamayowa@users.noreply.github.com>
2024-09-09 16:48:06 +0100

    Injected icon element with javascript, from the API.
    Removed image and created a new div.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ function refreshWeather(response) {
     let windElement = document.querySelector("#wind");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-
-    console.log(response.data);
+    let iconElement = document.querySelector("#icon");
 
     
     cityElement.innerHTML = response.data.city;
@@ -16,6 +15,7 @@ function refreshWeather(response) {
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temp-icon" />`
  
     temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
 }
